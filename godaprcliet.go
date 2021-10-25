8d095a9830459c20838623a3ee44ab5ccba7f8a7
mahesh <mail.mahesh.lk@gmail.com>
2021-10-25 19:56:40 +0800

    go dapr client

--- godaprcliet.go
+++ godaprcliet.go
@@ -7,3 +7,17 @@ import (
 
 	dapr "github.com/dapr/go-sdk/client"
 )
+
+func main() {
+
+	// just for this demo
+	ctx := context.Background()
+	data := []byte("ping")
+
+	// create the client
+	client, err := dapr.NewClient()
+	if err != nil {
+		log.Panic(err)
+	}
+	defer client.Close()
+}
