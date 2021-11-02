ea659e75726783bcb5e9d908fe987b334c34f78e
mahesh <mail.mahesh.lk@gmail.com>
2021-11-02 16:44:30 +0800

    go dapr client

--- callbackdapr/callback.go
+++ callbackdapr/callback.go
@@ -2,4 +2,12 @@ package main
 
 import (
 	daprd "github.com/dapr/go-sdk/service/grpc"
+	"log"
 )
+
+func main() {
+	s, err := daprd.NewService(":50001")
+	if err != nil {
+		log.Fatalf("failed to start the server: %v", err)
+	}
+}
